96aee187398a45ca17d586683f00d2d7964d5fe9
Shanya Connell <shanyaconnell@gmail.com>
2021-03-03 12:02:43 -0500

    Initial Commit - TCP/IP Client Server Single Interaction

--- Source Files/src/tcp/ip/Client.java
+++ Source Files/src/tcp/ip/Client.java
@@ -1,5 +1,26 @@
 package tcp.ip;
 
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class Client {
 
+    private static final String SERVERIP = "127.0.0.1";
+    private static final int SERVERPORT = 9097;
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket(SERVERIP, SERVERPORT);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String serverResponse = input.readLine();
+
+        JOptionPane.showMessageDialog(null, serverResponse);
+
+        socket.close();
+        System.exit(0);
+    }
 }
--- Source Files/src/tcp/ip/Server.java
+++ Source Files/src/tcp/ip/Server.java
@@ -1,4 +1,26 @@
 package tcp.ip;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+
+    private static final int PORT = 9097;
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket listener = new ServerSocket(PORT);
+
+        System.out.println("Server is waiting for client connection");
+        Socket client = listener.accept();
+
+        System.out.println("Server connected to client");
+        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+        out.println("I am a Server");
+        System.out.println("Server sent message. Closing.");
+
+        client.close();
+        listener.close();
+    }
 }
