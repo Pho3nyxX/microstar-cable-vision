2c3ba5670311e55f10fe03a649704c1566fb5b29
Abi Matthews-Gordon <60277772+AbiMatthews-Gordon@users.noreply.github.com>
2021-03-03 21:54:22 -0500

    Applications created, architecture diagram added
    Created client server and common applications. Created architecture diagram showing project structure.

---
+++ Common/src/Util.java
@@ -0,0 +1,7 @@
+/**
+ * Class Util acts as a utility class which stores often/used code 
+ * that may be required in multiple places across the server and client alike.
+ */
+public class Util {
+    
+}
