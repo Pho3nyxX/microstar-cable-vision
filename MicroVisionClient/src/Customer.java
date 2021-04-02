f1d6725cf5e0439ccc5c39adcf49348facc3c558
Shanya Connell <shanyaconnell@gmail.com>
2021-04-02 17:20:02 -0500

    Live Chat

--- MicroVisionClient/src/Customer.java
+++ MicroVisionClient/src/Customer.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 
+import models.chat._Message;
 import models.users._Customer;
 
 import utilities.ServerRequest;
@@ -7,10 +8,25 @@ import utilities.ServerResponse;
 
 public class Customer extends _Customer implements Serializable{
 
+    public void liveChat (_Message message) {
+        //Login to live chat
+        ServerRequest<_Message> request = new ServerRequest<_Message>(ServerRequest.USER_LIVE_CHAT_COMMAND, message);
+        App.messageConnection.sendAction(request);
+    }
+
     @Override
     public void LodgeComplaint() {
         // TODO Auto-generated method stub
         
     }
-    
+
+    @Override
+    public boolean save() {
+        return false;
+    }
+
+    @Override
+    public boolean delete() {
+        return false;
+    }
 }
