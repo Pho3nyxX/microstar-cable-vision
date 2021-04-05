a1617c1fd5daf5c9119baeab9f2290a559bebf0d
travelandcode <78379988+travelandcode@users.noreply.github.com>
2021-04-05 00:18:50 -0500

    Update ComplaintForm.java
    Design was updated. Working on making submit store the complaint on the database

--- MicroVisionClient/src/views/customer/ComplaintForm.java
+++ MicroVisionClient/src/views/customer/ComplaintForm.java
@@ -1,136 +1,93 @@
 package views.customer;
 
+import javax.swing.border.EmptyBorder;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JButton;
-import javax.swing.JComboBox;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.swing.SwingConstants;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+
+import java.awt.Font;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollBar;
+import java.awt.Color;
+import java.awt.EventQueue;
 
-public class ComplaintForm extends JPanel {
+public class ComplaintForm extends JFrame {
 
-    /**-------------------------MEMBERS------------------------------- */
-    //inputs
-    JTextArea lodgeComplaintTxtArea;
-    JButton submitBtn;
-    JButton cancelBtn;
-    
-    // labels
-    JLabel selectTypeOfServiceLabel;
-    JLabel lodgeComplaintLabel;
-    JLabel selectMethodOfContactLabel;
-    JLabel MicroStarLabel;
-    JLabel ComplaintFormLabel;
-    
-    //comboboxes
-    JComboBox<String> selectTypeOfServiceCombobox;
-    JComboBox<String> selectMethodOfContactCombobox;
-    
-    // combobox options
-    String typesOfService[] = { "Internet", "Phone", "Television" };
-    String methodsOfContact[] = { "Phone", "Email" };
-    
-    // containers
-    JScrollPane lodgeComplaintScroller;
+	private JPanel contentPane;
+	private JLabel type_of_complaint_label;
 
+	/**
+	 * Create the frame.
+	 */
+	public ComplaintForm() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\18764\\Downloads\\starlogo.jpg"));
+		setTitle("Micro Star Vision");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 477, 341);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel FormLabel = new JLabel("Complaint Form");
+		FormLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
+		FormLabel.setBounds(5, 5, 450, 16);
+		FormLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		contentPane.add(FormLabel);
+		
+		type_of_complaint_label = new JLabel("Type of Complaint");
+		type_of_complaint_label.setFont(new Font("Tahoma", Font.BOLD, 12));
+		type_of_complaint_label.setBounds(15, 44, 119, 27);
+		contentPane.add(type_of_complaint_label);
+		
+		JComboBox type_of_complaint_combobox = new JComboBox();
+		type_of_complaint_combobox.setModel(new DefaultComboBoxModel(new String[] {"Internet", "Phone", "Television"}));
+		type_of_complaint_combobox.setMaximumRowCount(3);
+		type_of_complaint_combobox.setBounds(152, 45, 285, 25);
+		contentPane.add(type_of_complaint_combobox);
+		
+		JLabel complaint_label = new JLabel("Complaint");
+		complaint_label.setFont(new Font("Tahoma", Font.BOLD, 12));
+		complaint_label.setBounds(15, 125, 88, 27);
+		contentPane.add(complaint_label);
+		
+		JTextArea complaint_textarea = new JTextArea();
+		complaint_textarea.setBounds(121, 100, 327, 93);
+		contentPane.add(complaint_textarea);
+		
+		JComboBox contact_method_combobox = new JComboBox();
+		contact_method_combobox.setModel(new DefaultComboBoxModel(new String[] {"Phone ", "Email"}));
+		contact_method_combobox.setMaximumRowCount(2);
+		contact_method_combobox.setBounds(152, 213, 285, 25);
+		contentPane.add(contact_method_combobox);
+		
+		JLabel contact_method_label = new JLabel("Contact Method");
+		contact_method_label.setFont(new Font("Tahoma", Font.BOLD, 12));
+		contact_method_label.setBounds(12, 217, 110, 16);
+		contentPane.add(contact_method_label);
+		
+		JButton submitBtn = new JButton("Submit");
+		submitBtn.setBounds(185, 264, 98, 26);
+		contentPane.add(submitBtn);
+		
+		submitBtn.addActionListener(new ActionListener() {
 
-    /**-------------------------CONSTRUCTORS------------------------------- */
-    public ComplaintForm() {
-
-        // JLabel objects
-        MicroStarLabel = new JLabel("Micro-Star Cable-Vision");
-        ComplaintFormLabel = new JLabel("Complaint Form");
-
-
-        // text fields objects
-        lodgeComplaintTxtArea = new JTextArea();
-        lodgeComplaintScroller = new JScrollPane();
-        submitBtn = new JButton("Submit");
-        cancelBtn = new JButton("Cancel");
-
-
-        // JLabel objects
-        selectTypeOfServiceLabel = new JLabel("Select Type of Service");
-        lodgeComplaintLabel = new JLabel("Lodge Complaint");
-        selectMethodOfContactLabel = new JLabel("Select Contact Method");
-
-
-        // a string array of service list items
-        selectTypeOfServiceCombobox = new JComboBox<>(typesOfService);
-        selectMethodOfContactCombobox = new JComboBox<>(methodsOfContact);
-
-
-        // set one of the options as selected
-        selectTypeOfServiceCombobox.setSelectedItem(0);
-        selectMethodOfContactCombobox.setSelectedItem(0);
-
-
-        // setting the size of the labels
-        selectTypeOfServiceLabel.setBounds(10, 100, 150, 20);
-        lodgeComplaintLabel.setBounds(10, 150, 150, 20);
-        selectMethodOfContactLabel.setBounds(10, 250, 150, 20);
-
-
-        // setting the size of text field sizes
-        MicroStarLabel.setBounds(00, 0, 350, 50);
-        ComplaintFormLabel.setBounds(50, 30, 250, 50);
-
-        selectTypeOfServiceCombobox.setBounds(150, 90, 200, 40);
-        lodgeComplaintTxtArea.setBounds(0, 0, 150, 100); // TODO:: fix scroll pane
-        lodgeComplaintScroller.setBounds(150, 140, 200, 100); // TODO:: fix scroll pane
-        selectMethodOfContactCombobox.setBounds(150, 250, 200, 40);
-        submitBtn.setBounds(150, 300, 95, 40);
-        cancelBtn.setBounds(255, 300, 95, 40);
-
-
-        // adding action listener to Submit button because it requires an action if it
-        // is selected
-        submitBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Button clicked");
-            }
-        });
-
-        // adding action listener to Cancel button because it requires an action if it
-        // is selected
-        cancelBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Button clicked");
-            }
-        });
-
-        // centering Micro-Star Cable-Vision and Complaint Form
-        MicroStarLabel.setHorizontalAlignment(JLabel.CENTER);
-        ComplaintFormLabel.setHorizontalAlignment(JLabel.CENTER);
-
-        // adding attributes
-        lodgeComplaintScroller.add(lodgeComplaintTxtArea);
-        
-        this.add(MicroStarLabel);
-        this.add(ComplaintFormLabel);
-        this.add(selectTypeOfServiceCombobox);
-        this.add(lodgeComplaintScroller); // TODO:: fix scroll pane
-        this.add(selectMethodOfContactCombobox);
-        this.add(submitBtn);
-        this.add(cancelBtn);
-
-        // adding labels to panel
-        this.add(selectTypeOfServiceLabel);
-        this.add(lodgeComplaintLabel);
-        this.add(selectMethodOfContactLabel);
-
-        this.setLayout(null);
-
-        // making GUI visable
-        this.setVisible(true);
-
-    }
-
-    
-
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				System.out.println("Button was clicked");
+				System.out.println("Button was clicked");
+				System.out.println("Button was clicked");
+			}
+			
+		});
+	}
 }
