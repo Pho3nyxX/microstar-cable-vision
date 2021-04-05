f59f16cfed5dc180a7ab1746ccd6ebfe7c2df05d
travelandcode <78379988+travelandcode@users.noreply.github.com>
2021-04-05 06:48:43 -0500

    Update IfSave.java

--- MicroVisionClient/src/IfSave.java
+++ MicroVisionClient/src/IfSave.java
@@ -1,6 +1,7 @@
 package views.customer;
 
 import java.awt.BorderLayout;
+import java.awt.Cursor;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -9,6 +10,9 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
 import javax.swing.JButton;
 
 public class IfSave extends JFrame {
@@ -48,18 +52,62 @@ public class IfSave extends JFrame {
 		ThankYou_Label.setBounds(10, 23, 543, 61);
 		contentPane.add(ThankYou_Label);
 		
-		JButton ViewComplaintBtn = new JButton("View Complaint");
-		ViewComplaintBtn.setBounds(121, 176, 141, 34);
-		contentPane.add(ViewComplaintBtn);
-		
-		JButton ReturntoDashboardBtn = new JButton("Return to dashboard");
-		ReturntoDashboardBtn.setBounds(342, 174, 141, 38);
-		contentPane.add(ReturntoDashboardBtn);
-		
 		JLabel Asap_Label = new JLabel("as soon as possible.");
 		Asap_Label.setHorizontalAlignment(SwingConstants.CENTER);
 		Asap_Label.setFont(new Font("Tahoma", Font.BOLD, 13));
 		Asap_Label.setBounds(215, 81, 141, 23);
 		contentPane.add(Asap_Label);
+		
+		JLabel ViewComplaintLabel = new JLabel("View Complaint");
+		ViewComplaintLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		ViewComplaintLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		ViewComplaintLabel.setBounds(111, 197, 116, 23);
+		contentPane.add(ViewComplaintLabel);
+		
+		ViewComplaintLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		ViewComplaintLabel.addMouseListener(new MouseAdapter() {
+			 
+		    @Override
+		    public void mouseClicked(MouseEvent e) {
+		        // the user clicks on the label
+		    	System.out.println("Forgot button works");
+		    }
+		 
+		    @Override
+		    public void mouseEntered(MouseEvent e) {
+		        // the mouse has entered the label
+		    }
+		 
+		    @Override
+		    public void mouseExited(MouseEvent e) {
+		        // the mouse has exited the label
+		    }
+		});
+		
+		JLabel DashboardLabel = new JLabel("Return to Dashboard");
+		DashboardLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		DashboardLabel.setFont(new Font("Tahoma", Font.BOLD, 13));
+		DashboardLabel.setBounds(335, 197, 146, 23);
+		contentPane.add(DashboardLabel);
+		
+		DashboardLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		DashboardLabel.addMouseListener(new MouseAdapter() {
+			 
+		    @Override
+		    public void mouseClicked(MouseEvent e) {
+		        // the user clicks on the label
+		    	System.out.println("Forgot button works");
+		    }
+		 
+		    @Override
+		    public void mouseEntered(MouseEvent e) {
+		        // the mouse has entered the label
+		    }
+		 
+		    @Override
+		    public void mouseExited(MouseEvent e) {
+		        // the mouse has exited the label
+		    }
+		});
 	}
 }
