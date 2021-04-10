166db21c60eed8eea6f833a3fc1d2d64aea904db
Abi Matthews-Gordon <60277772+AbiMatthews-Gordon@users.noreply.github.com>
2021-04-09 21:54:03 -0500

    Login Dialog created

--- MicroVisionClient/src/views/customer/SignIn.java
+++ MicroVisionClient/src/views/customer/SignIn.java
@@ -2,24 +2,36 @@ package views.customer;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.Color;
+import java.awt.Frame;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class SignIn extends JPanel{
+import controllers.Authentication;
+
+public class SignIn extends JDialog{
 
     JTextField usernameTxt;
     JPasswordField passwordTxt;
     JButton loginBtn;
     JButton forgetPasswordBtn;
+    JPanel contentPanel;
+    JLabel loginErrorLabel;
+    JLabel microStarLabel;
 
+    /*----------------------CONSTRUCTORS-----------------------------*/
 
-    public SignIn() {
+    public SignIn(Frame parent) {
 
-        JLabel MicroStarLabel = new JLabel("Micro-Star Cable-Vision");
+        super(parent, "Sign in", true);
+
+        contentPanel = new JPanel();
+        microStarLabel = new JLabel("Micro-Star Cable-Vision");
+        loginErrorLabel = new JLabel("Login failed");
         
         // text fields
         usernameTxt = new JTextField();
@@ -28,21 +40,41 @@ public class SignIn extends JPanel{
         forgetPasswordBtn = new JButton("Forget Password");
 
         // setting the size of text fields
-        MicroStarLabel.setBounds(00, 0, 350, 50);
+        microStarLabel.setBounds(0, 0, 315, 50);
+        loginErrorLabel.setBounds(0, 155, 315, 50);
 
-        MicroStarLabel.setHorizontalAlignment(JLabel.CENTER);
+        microStarLabel.setHorizontalAlignment(JLabel.CENTER);
+        loginErrorLabel.setHorizontalAlignment(JLabel.CENTER);
+        loginErrorLabel.setForeground(Color.RED);
+        loginErrorLabel.setVisible(false);
 
         // setting the sizes of the text fields
         usernameTxt.setBounds(25,50,250,50);
-        passwordTxt.setBounds(25,110,250,50);
-        loginBtn.setBounds(25,180,250,50);
-        forgetPasswordBtn.setBounds(120,260,150,50);
+        passwordTxt.setBounds(25,120,250,50);
+        loginBtn.setBounds(25,200,250,50);
+        forgetPasswordBtn.setBounds(120,280,150,25);
+        forgetPasswordBtn.setBackground(null);
 
-        // adding action listener to Forget Password button because it requires an action if it is selected
+        // adding action listener to button because it requires an action if it is selected
         loginBtn.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e){
-                System.out.println("Button clicked");
+                // System.out.println("Button clicked");
+                Authentication auth =  new Authentication();
+                String password;
+                String userName;
+                boolean loggedIn = false;
+
+                password = new String(passwordTxt.getPassword());
+                userName = usernameTxt.getText();
+                
+                loggedIn = auth.login(userName, password);
+
+                if (loggedIn) {
+                    
+                } else {
+                    loginErrorLabel.setVisible(true);;
+                }
             }
         });
         
@@ -53,26 +85,29 @@ public class SignIn extends JPanel{
                 System.out.println("Button clicked");
             }
         });
+        contentPanel.setBounds(0,0,300,300);
         
         // adding attributes 
-        this.add(MicroStarLabel);
-        this.add(usernameTxt);
-        this.add(passwordTxt);
-        this.add(loginBtn);
-        this.add(forgetPasswordBtn);
+        contentPanel.add(microStarLabel);
+        contentPanel.add(loginErrorLabel);
+        contentPanel.add(usernameTxt);
+        contentPanel.add(passwordTxt);
+        contentPanel.add(loginBtn);
+        contentPanel.add(forgetPasswordBtn);
         
-        this.setLayout(null);
         
-        // making gui visable
+        this.add(contentPanel);
+        this.setSize(315,400);
+        contentPanel.setLayout(null);
+        
+        // making gui visable'
+        // this.setResizable(false);
+        this.setLocationRelativeTo(parent);
+        // this.pack();
         this.setVisible(true);
     }
 
-    public SignIn(JTextField usernameTxt, JPasswordField passwordTxt, JButton loginBtn, JButton forgetPasswordBtn) {
-        this.usernameTxt = usernameTxt;
-        this.passwordTxt = passwordTxt;
-        this.loginBtn = loginBtn;
-        this.forgetPasswordBtn = forgetPasswordBtn;
-    }
+
 
     public JTextField getUsernameTxt() {
         return this.usernameTxt;
